00cf5b7bf6b7353822a07ca9978282dea068e947
IE-CH <106725190+iech2997@users.noreply.github.com>
2024-08-10 01:25:37 +0100

    modified class and id names

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 async function get_major_apod() {
-    let major_apod_url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
+    //let major_apod_url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
+    let major_apod_url = "https://api.nasa.gov/planetary/apod?api_key=L2tecoNEQagoNEzVxASkC2tpVN8ZjgnxGeu0BWqB";
 
     // The fetch() function returns a Promise which is fulfilled with a Response object representing the server's response.
     let major_apod_response = await fetch(major_apod_url);
@@ -25,31 +26,39 @@ async function get_major_apod() {
     if (major_apod_response_json.hasOwnProperty("explanation")) {
         document.getElementById("major-explanation").innerHTML = major_apod_response_json.explanation;
     }
-    console.log(major_apod_response_json);
 }
 
 async function get_minor_apod() {
     let current_datetime = new Date();
-    for (let i = 1; i <= 4; i++) {
-        current_datetime.setDate(current_datetime.getDate() - 1);
-        let year = current_datetime.getFullYear();
-        let month = current_datetime.getMonth() + 1;
-        let date = current_datetime.getDate();
-        let minor_apod_url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY=" + year + "-" + month + "-" + date;
-        let minor_apod_response = await fetch(minor_apod_url);
-        let minor_apod_response_json = await minor_apod_response.json();
-        if (minor_apod_response_json.hasOwnProperty("date")) {
-            document.getElementById("minor" + i + "-date").innerHTML = minor_apod_response_json.date;
+    current_datetime.setDate(current_datetime.getDate() - 1);
+    let end_year = current_datetime.getFullYear();
+    let end_month = current_datetime.getMonth() + 1;
+    let end_date = current_datetime.getDate();
+    end = end_year + "-" + end_month + "-" + end_date;
+    
+    current_datetime.setDate(current_datetime.getDate() - 3);  // already deducted 1 from previous
+    let start_year = current_datetime.getFullYear();
+    let start_month = current_datetime.getMonth() + 1;
+    let start_date = current_datetime.getDate();
+    start = start_year + "-" + start_month + "-" + start_date;
+    
+    let minor_apod_url = "https://api.nasa.gov/planetary/apod?api_key=L2tecoNEQagoNEzVxASkC2tpVN8ZjgnxGeu0BWqB&start_date=" + start + "&end_date=" + end;
+    let minor_apod_response = await fetch(minor_apod_url);
+    let minor_apod_response_json = await minor_apod_response.json();
+    
+    for (let i = 4; i >= 1 ; i--) {
+        if (minor_apod_response_json[i-1].hasOwnProperty("date")) {
+            document.getElementById("minor" + i + "-date").innerHTML = minor_apod_response_json[4-i].date;
         }
-        if (minor_apod_response_json.hasOwnProperty("title")) {
-            document.getElementById("minor" + i + "-title").innerHTML = minor_apod_response_json.title;
+        if (minor_apod_response_json[4-i].hasOwnProperty("title")) {
+            document.getElementById("minor" + i + "-title").innerHTML = minor_apod_response_json[4-i].title;
         }
-        if (minor_apod_response_json.hasOwnProperty("media_type") && minor_apod_response_json.hasOwnProperty("url")){
-            if (minor_apod_response_json.media_type == "video") {
-                document.getElementById("minor" + i + "-video").src = minor_apod_response_json.url;
+        if (minor_apod_response_json[4-i].hasOwnProperty("media_type") && minor_apod_response_json[4-i].hasOwnProperty("url")){
+            if (minor_apod_response_json[4-i].media_type == "video") {
+                document.getElementById("minor" + i + "-video").src = minor_apod_response_json[4-i].url;
             }
-            else if (minor_apod_response_json.media_type == "image") {
-                document.getElementById("minor" + i + "-picture").src = minor_apod_response_json.url;
+            else if (minor_apod_response_json[4-i].media_type == "image") {
+                document.getElementById("minor" + i + "-picture").src = minor_apod_response_json[4-i].url;
                 const element = document.getElementById("minor" + i + "-video");
                 element.remove();
             }
