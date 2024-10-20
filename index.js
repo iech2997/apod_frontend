851a513efb66a2579ad63b812c6d03b992b5a9a9
EE <106725190+iech2997@users.noreply.github.com>
2024-10-20 19:58:40 +0100

    single api call

--- index.js
+++ index.js
@@ -1,79 +1,60 @@
-async function get_major_apod() {
-    let major_apod_url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
+const apodContainer = document.getElementById("apod-container");
+const loadMoreBtn = document.getElementById("load-more-btn");
+let startingIndex = 0;
+let endingIndex = 7;
+let apodDataArr = [];
+let currentDatetime = new Date();
+const apodBaseUrl = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
 
-    // The fetch() function returns a Promise which is fulfilled with a Response object representing the server's response.
+async function GetApod() {
     // The fetch() method returns a Promise, which is a placeholder object that will either be "fulfilled" if your request is successful, 
     // or "rejected" if your request is unsuccessful.
-    // If the Promise is fulfilled, it resolves to a Response object, and you can use the .then() method to access the Response. 
+    // If the Promise is fulfilled, it resolves to a Response object.
+    // You can then handle the response by using methods like .json(), .text(), or .blob() on the response object to read the data
     // ** using async/await instead of .then() is preferred here 
-    let major_apod_response = await fetch(major_apod_url);
+    let apodUrl = `${apodBaseUrl}&start_date=${GetDateString(15)}`;
+    let apodRes = await fetch(apodUrl);
+
     // The data you get from a GET request is not usable at first. To make the data usable, you can use the .json() method on the Response object
     // Response.json() Returns a promise that is the result of taking JSON as input and parsing it to produce a JavaScript object.
-    let major_apod_response_json = await major_apod_response.json();
-    document.getElementById("major-date").innerHTML = "Today";
-    if (major_apod_response_json.hasOwnProperty("title")) {
-        document.getElementById("major-title").innerHTML = major_apod_response_json.title;
-    }
-    if (major_apod_response_json.hasOwnProperty("media_type") && major_apod_response_json.hasOwnProperty("url")){
-        if (major_apod_response_json.media_type == "video") {
-            document.getElementById("major-video").src = major_apod_response_json.url;
-            const pictureelement = document.getElementById("major-picture");
-            pictureelement.remove();
-        }
-        else if (major_apod_response_json.media_type == "image") {
-            document.getElementById("major-picture").src = major_apod_response_json.url;
-            const element = document.getElementById("major-video");
-            element.remove();
-        }
-    }
-    if (major_apod_response_json.hasOwnProperty("copyright")) {
-        document.getElementById("major-copyright").innerHTML = major_apod_response_json.copyright;
-    }
-    if (major_apod_response_json.hasOwnProperty("explanation")) {
-        document.getElementById("major-explanation").innerHTML = major_apod_response_json.explanation;
-    }
+    apodDataArr = await apodRes.json();
+    apodDataArr.reverse();
+    DisplayApod(apodDataArr.slice(startingIndex, endingIndex));
 }
 
-async function get_minor_apod() {
-    let current_datetime = new Date();
-    current_datetime.setDate(current_datetime.getDate() - 1);
-    let end_year = current_datetime.getFullYear();
-    let end_month = current_datetime.getMonth() + 1;
-    let end_date = current_datetime.getDate();
-    end = end_year + "-" + end_month + "-" + end_date;
-    
-    current_datetime.setDate(current_datetime.getDate() - 3);  // already deducted 1 from previous
-    let start_year = current_datetime.getFullYear();
-    let start_month = current_datetime.getMonth() + 1;
-    let start_date = current_datetime.getDate();
-    start = start_year + "-" + start_month + "-" + start_date;
-    
-    let minor_apod_url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&start_date=" + start + "&end_date=" + end;
-    let minor_apod_response = await fetch(minor_apod_url);
-    let minor_apod_response_json = await minor_apod_response.json();
-    
-    for (let i = 4; i >= 1 ; i--) {
-        if (minor_apod_response_json[i-1].hasOwnProperty("date")) {
-            document.getElementById("minor" + i + "-date").innerHTML = minor_apod_response_json[4-i].date;
-        }
-        if (minor_apod_response_json[4-i].hasOwnProperty("title")) {
-            document.getElementById("minor" + i + "-title").innerHTML = minor_apod_response_json[4-i].title;
-        }
-        if (minor_apod_response_json[4-i].hasOwnProperty("media_type") && minor_apod_response_json[4-i].hasOwnProperty("url")){
-            if (minor_apod_response_json[4-i].media_type == "video") {
-                document.getElementById("minor" + i + "-video").src = minor_apod_response_json[4-i].url;
-                const pictureelement = document.getElementById("minor" + i + "-picture");
-                pictureelement.remove();
-            }
-            else if (minor_apod_response_json[4-i].media_type == "image") {
-                document.getElementById("minor" + i + "-picture").src = minor_apod_response_json[4-i].url;
-                const element = document.getElementById("minor" + i + "-video");
-                element.remove();
-            }
-        }
+function FetchMoreApod() {
+    startingIndex += 7;
+    endingIndex += 7;
+    DisplayApod(apodDataArr.slice(startingIndex, endingIndex));
+    if (apodDataArr.length <= endingIndex) {
+        loadMoreBtn.disabled = true;
+        loadMoreBtn.textContent = "No more data to load";
     }
+};
+
+function DisplayApod(apodCollection) {
+    // Destructuring parameters is a convenient way to extract values from objects and arrays directly within function parameters
+    apodCollection.forEach(({copyright, date, explanation, title, url}, index) => {
+        apodContainer.innerHTML += `
+            <div id="${index}" class="apod-card">
+                <img class=""apod-img" src="${url}" alt="${title} image">
+                <h2 class="apod-title">${title}</h2>
+                <p class="copyright">${copyright}</p>
+                <p class="date">${date}</p>
+                <p class="explanation">${explanation}</p>
+            </div>
+        `;
+    });
 }
 
-// main
-get_major_apod();
-get_minor_apod();
+function GetDateString(daysAgo) {
+    currentDatetime.setDate(currentDatetime.getDate() - daysAgo);
+    let year = currentDatetime.getFullYear();
+    let month = currentDatetime.getMonth() + 1;
+    let date = currentDatetime.getDate();
+    return `${year}-${month}-${date}`;
+}
+
+loadMoreBtn.addEventListener("click", FetchMoreApod);
+
+GetApod();
