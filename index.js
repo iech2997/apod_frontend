225f2d71fbe130cc29fcc62f13a8e2e56d05d73e
EE <106725190+iech2997@users.noreply.github.com>
2024-11-19 23:11:42 +0000

    add updateCommit function

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
+import { Octokit } from "https://esm.sh/@octokit/core";
+
+
+const octokit = new Octokit({
+    auth: 'DEMO'
+});
+const githubOwner = "iech2997";
+const githubRepo = "apod_frontend";
+
+
 const apodContainer = document.getElementById("apod-container");
 const loadMoreBtn = document.getElementById("load-more-btn");
 const headerSearch = document.querySelector("#header-search");
+const githubCommit = document.querySelector("#commit-btn");
 let endDaysAgo = 0;
 let startDaysAgo = 6;
 let currentIndex = 0;
@@ -10,21 +21,42 @@ const apodBaseUrl = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
 
 loadMoreBtn.addEventListener("click", FetchMoreApod);
 headerSearch.addEventListener("keyup", Search);
+githubCommit.addEventListener("click", updateCommit);
 
 GetApod(startDaysAgo, endDaysAgo);
 
+updateCommit();
+
+
+async function updateCommit() {
+    githubCommit.innerText = await getGithubCommit();
+}
+
+async function getGithubCommit() {   
+    return 10;
+
+    let commitRes = await octokit.request('GET /repos/{owner}/{repo}/commits', {
+        owner: githubOwner,
+        repo: githubRepo,
+        headers: {
+            'X-GitHub-Api-Version': '2022-11-28'
+        }
+    });
+    
+    return commitRes["data"].length;
+}
 
 async function GetApod(start, end) {
     // The fetch() method returns a Promise, which is a placeholder object that will either be "fulfilled" if your request is successful, 
     // or "rejected" if your request is unsuccessful.
-    // If the Promise is fulfilled, it resolves to a Response object.
-    // You can then handle the response by using methods like .json(), .text(), or .blob() on the response object to read the data
+    // If the fetch() promise is fulfilled, it resolves to a Response object.
+    // You can then handle the Response object by using methods like .json(), .text(), or .blob() on the response object to read the data
     // ** using async/await instead of .then() is preferred here 
     let apodUrl = `${apodBaseUrl}&start_date=${GetDateString(start)}&end_date=${GetDateString(end)}`;
     let apodRes = await fetch(apodUrl);
 
-    // The data you get from a GET request is not usable at first. To make the data usable, you can use the .json() method on the Response object
-    // Response.json() Returns a promise that is the result of taking JSON as input and parsing it to produce a JavaScript object.
+    // The data you get from a GET request is not usable at first. To make the data usable, you can use the Response object's .json() method
+    // Response.json() returns a Promise that resolves to a JavaScript object.
     apodDataArr = await apodRes.json();
     apodDataArr.reverse();
     DisplayApod(apodDataArr);
@@ -40,14 +72,13 @@ function FetchMoreApod() {
 
 function DisplayApod(apodCollection) {
     // Destructuring parameters is a convenient way to extract values from objects and arrays directly within function parameters
-    apodCollection.forEach(({copyright, date, explanation, title, url, media_type}) => {
+    apodCollection.forEach(({ copyright, date, explanation, title, url, media_type }) => {
         apodContainer.innerHTML += `<article id="apod-card-${currentIndex}" class="apod-card"></article>`;
-        
+
         let apodCard = document.getElementById("apod-card-" + currentIndex);
-        
+
         let mediaHtml = "";
-        if (url)
-        {
+        if (url) {
             if (media_type == "image") {
                 mediaHtml = `<img class="apod-media" src="${url}">`;
             }
